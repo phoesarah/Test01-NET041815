93ecb85c80dbf17124de69af788c736897712493
Sarah R <phoesarah@hotmail.com>
2015-05-30 12:08:14 -0500

    Test

--- Test01/TestObject.cs
+++ Test01/TestObject.cs
@@ -10,25 +10,50 @@ namespace Test01
         internal List<int> Reduce(List<int> input)
         {
             List<int> result = new List<int>();
-            for (int i = 0; i < 5; i++)
+
+            if (input.Count > 5)
             {
-                if ((input[i] >= 4) && (input[i] <10))
+                for (int i = 0; i < 5; i++)
                 {
-                  
-                    result.Add(input[i]);
+
+                    {
+                        result.Add(input[i]);
+                    }
                 }
-                               
             }
+            else
+                for (int i = 0; i < input.Count; i++)
+                {
 
-            //for (int i = 0; i < result.Count; i++)
+                    {
+                        result.Add(input[i]);
+                    }
+                }
+                        
+            result = result.Where(x => x >= 4 && x < 10).Distinct().ToList();
+           
+
+            //for (int i = 0; i < 5; i++)
             //{
-            //    if ((input[i] >= 4) && (input[i] < 10))
+            //    if ((input[i] >= 4) && (input[i] <10))
             //    {
-
+                  
             //        result.Add(input[i]);
             //    }
-
+                               
             //}
+
+           //List<int> Copy = result.GroupBy(x => x).ToList();
+           // for (int i = 1; i < result.Count; i++)
+           // {
+           //  if (result[i] == result[i-1])
+           //  {
+           //      result.Remove(result[i]);
+           //  }
+
+           // }
+            
+
             return result;
         }
 
